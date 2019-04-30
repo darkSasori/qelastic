bce2e51148074005d043206f2e0ea8b1154c55ff
Lineu Felipe <lineufelipe@gmail.com>
2019-04-29 21:29:00 -0300

    new layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QTextEdit>
+#include <QTextList>
+#include <QTextListFormat>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
